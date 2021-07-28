a994d74911d9f73f0ced7ab625ccf3826f2cd0a6
Fernando Pereyra <percuafmx@hotmail.com>
2021-07-27 21:18:54 -0500

    Utilizando funciones

--- figuras.js
+++ figuras.js
@@ -1,55 +1,51 @@
-const ladoCuadrado = 5;
-
 console.group("Cuadrados");
-console.log("Los lados del cuadrado miden " + ladoCuadrado + "cm");
 
-perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perímetro del cuadrado es: " + perimetroCuadrado + "cm");
+function perimetroCuadrado(lado) {
+    return lado * 4;
+}
 
-areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log("El área del cuadrado es: " + areaCuadrado + "cm²");
+function areaCuadrado(lado){
+    return lado * lado;    
+}
+
 console.groupEnd();
 
 
 console.group("Triángulos");
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
 
-console.log("los lados del triángulo miden: " + ladoTriangulo1 + "cm " + ladoTriangulo2 + "cm " + baseTriangulo + "cm " );
+function perimetroTriangulo(lado1, lado2, base){
+    return lado1 + lado2 + base;    
+} 
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-console.log("El perímetro del triangulo es: " + perimetroTriangulo + "cm")
+function altura(base, lado2){
+    baseDividida = base/2;
+    hipotenusa = Math.pow(lado2, 2);
+    basePitagoras = Math.pow(baseDividida, 2);
+    primerProcesoPitagoras = hipotenusa - basePitagoras;
+    return Math.sqrt(primerProcesoPitagoras);    
+}
 
-const baseTrianguloDividida = baseTriangulo/2;
+function areaTriangulo(base, lado2){
+    return (base * altura(base, lado2))/2;
+}
 
-let hipotenusaTriangulo = Math.pow(ladoTriangulo2, 2);
-let basePitagoras = Math.pow(baseTrianguloDividida, 2);
-
-const PrimerProcesoPitagoras = hipotenusaTriangulo - basePitagoras;
-
-const ResultadoFinalPitagoras = Math.sqrt(PrimerProcesoPitagoras);
-
-console.log("El altura del triángulo es de: " + ResultadoFinalPitagoras + "cm");
-
-const areaTriangulo = (baseTriangulo * ResultadoFinalPitagoras)/2;
-console.log("El área final del triángulo es de: " + areaTriangulo + "cm²");
 console.groupEnd();
 
-
 console.group("Círculos")
+
+function diametroCirculo(radio){
+    return radio * 2;
+}
+
 const pi = Math.PI;
-const radioCirculo = 4;
-console.log("Pi es equivalente a: " + pi);
 
-console.log("El radio del círculo es de: " + radioCirculo + "cm");
+function perimetroCirculo(radio){
+    diametro = diametroCirculo(radio);   
+    return diametro * pi;
+}
 
-const diametroCirculo = radioCirculo * 2;
-const perimetroCirculo = diametroCirculo * pi;
-console.log("El díametro del círculo es de: " + diametroCirculo + "cm");
-console.log("El perímetro del círculo es de: " + perimetroCirculo + "cm ");
+function areaCirculo(radio){
+    return (radio * radio) * pi;
+}
 
-
-const areaCirculo = (radioCirculo * radioCirculo) * pi;
-console.log("El área del círculo es de: " + areaCirculo + "cm²");
 console.groupEnd();
