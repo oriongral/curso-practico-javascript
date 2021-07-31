0929797f74dc0968623cf63eb4386b3d2e1eb9ea
Fernando Pereyra <percuafmx@hotmail.com>
2021-07-31 17:22:38 -0500

    Descuentos con cupones usando if y arrays

--- descuentos.js
+++ descuentos.js
@@ -1,3 +1,5 @@
+const coupons = ["10%","15%","20%","25%","40%"];
+
 function calcularPrecioConDescuento(precio, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento)/100;
@@ -13,38 +15,28 @@ function onclickButtonPriceDiscount(){
     const inputCoupon = document.getElementById("inputCoupon");
     const couponValue = inputCoupon.value; 
 
-    let couponDiscount;
-    let precioConDescuento;
+if(!coupons.includes(couponValue)){
+    alert("El cupón " + couponValue + " no es válido");
+}else if (couponValue === "10%"){
+    couponDiscount = 10;
+    precioConDescuento = calcularPrecioConDescuento(priceValue,couponDiscount);
+    resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
+}else if(couponValue === "15%"){
+    couponDiscount = 15;
+    precioConDescuento = calcularPrecioConDescuento(priceValue,couponDiscount);
+    resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
+}else if(couponValue === "20%"){
+    couponDiscount = 20;
+    precioConDescuento = calcularPrecioConDescuento(priceValue,couponDiscount);
+    resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
+}else if(couponValue === "25%"){
+    couponDiscount = 25;
+    precioConDescuento = calcularPrecioConDescuento(priceValue,couponDiscount);
+    resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
+}else if(couponValue === "40%"){
+    couponDiscount = 40;
+    precioConDescuento = calcularPrecioConDescuento(priceValue,couponDiscount);
+    resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
 
-    switch (couponValue){
-        case "10%":
-            couponDiscount = 10;
-            precioConDescuento = calcularPrecioConDescuento(priceValue,couponDiscount);
-            resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
-            break;
-        case "15%":
-            couponDiscount = 15;
-            precioConDescuento = calcularPrecioConDescuento(priceValue,couponDiscount);
-            resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
-            break;
-         case "20%":
-            couponDiscount = 20;
-            precioConDescuento = calcularPrecioConDescuento(priceValue,couponDiscount);
-            resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
-            break;
-        case "25%":
-            couponDiscount = 25;
-            precioConDescuento = calcularPrecioConDescuento(priceValue,couponDiscount);
-            resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
-            break;
-        case "40%":
-            couponDiscount = 40;
-            precioConDescuento = calcularPrecioConDescuento(priceValue,couponDiscount);
-            resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
-            break;
-        default:
-            alert("El cupón ingresado no es valido");
-    }
-
- 
 }
