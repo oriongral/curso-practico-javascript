559eb2acc87db1f65c5f5cf62eac96d3ee9cf7bb
Fernando Pereyra <percuafmx@hotmail.com>
2021-08-01 23:49:18 -0500

    Mediana con el método sort

--- mediana.js
+++ mediana.js
@@ -1,41 +1,39 @@
-const lista1 = [
-    100,
-    200,
-    500,
-    400000000,
-];
+function calcularMediana(lista) {
+  const listaOrdenada = lista.sort(function (a, b) {
+    return a - b;
+  });
 
-function calcularMediaAritmetica(lista){    
-        const sumaLista = lista.reduce(
-            function(valorAcumulado = 0, nuevoElemento ){
-                return valorAcumulado + nuevoElemento;
-            }
-        );
-        const promedioLista = sumaLista / lista.length;
-        return promedioLista;
+  const mitadLista = parseInt(listaOrdenada.length / 2);
+
+  function esPar(numero) {
+    if (numero % 2 === 0) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
+  function calcularMediaAritmetica(listaOrdenada) {
+    const sumaLista = listaOrdenada.reduce(function (
+      valorAcumulado = 0,
+      nuevoElemento
+    ) {
+      return valorAcumulado + nuevoElemento;
+    });
+    const promedioLista = sumaLista / listaOrdenada.length;
+    return promedioLista;
+  }
 
-const mitadLista1 = parseInt(lista1.length /2);
+  let mediana;
 
-function esPar(numero){
-    if (numero % 2 === 0){
-        return true;
-    }else{
-        return false;
-    }
-}
-
-let mediana;
-
-if(esPar(lista1.length)){
-    const elemento1 = lista1[mitadLista1 -1];
-    const elemento2 = lista1[mitadLista1];
-    const promedioElementos = calcularMediaAritmetica([
-        elemento1,
-        elemento2,
-    ]);    
+  if (esPar(listaOrdenada.length)) {
+    const elemento1 = listaOrdenada[mitadLista - 1];
+    const elemento2 = listaOrdenada[mitadLista];
+    const promedioElementos = calcularMediaAritmetica([elemento1, elemento2]);
     mediana = promedioElementos;
-}else{
-    mediana = lista1[mitadLista1];
+  } else {
+    mediana = listaOrdenada[mitadLista];
+  }
+
+  return mediana;
 }
