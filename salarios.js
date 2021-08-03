91c4db0e7b83196ebb34465cc6cca3f00ec45edb
Fernando Pereyra <percuafmx@hotmail.com>
2021-08-03 17:46:13 -0500

    Top 10% de Salarios

--- salarios.js
+++ salarios.js
@@ -1,34 +1,3 @@
-const mexico = [];
-
-mexico.push({ name: "Adán", salary: 1000 });
-mexico.push({ name: "Omarf", salary: 960 });
-mexico.push({ name: "Irving", salary: 500 });
-mexico.push({ name: "Claudia", salary: 873 });
-mexico.push({ name: "Fernando", salary: 1200 });
-mexico.push({ name: "Alberto", salary: 1100 });
-mexico.push({ name: "Tania", salary: 300 });
-mexico.push({ name: "Elvis", salary: 700 });
-mexico.push({ name: "Adán", salary: 1000 });
-mexico.push({ name: "Guadalupe", salary: 780 });
-mexico.push({ name: "Rocío", salary: 500 });
-mexico.push({ name: "Emmanuel", salary: 964 });
-mexico.push({ name: "Vicente", salary: 440 });
-mexico.push({ name: "Joaquín", salary: 573 });
-mexico.push({ name: "Rubén", salary: 900 });
-mexico.push({ name: "Matusalén", salary: 1130 });
-mexico.push({ name: "Raquel", salary: 670 });
-mexico.push({ name: "Elvira", salary: 570 });
-mexico.push({ name: "Alfredo", salary: 400 });
-mexico.push({ name: "Gumercindo", salary: 678 });
-
-const salaryMex = mexico.map(function (persona) {
-  return persona.salary;
-});
-
-const salaryMexSorted = salaryMex.sort(function (salaryA, salaryB) {
-  return salaryA - salaryB;
-});
-
 function isPeer(number) {
   return number % 2 === 0;
 }
@@ -54,4 +23,51 @@ function medianSalary(list) {
   }
 }
 
-console.log(medianSalary(salaryMexSorted));
+const mexico = [];
+
+mexico.push({ name: "Adán", salary: 1000 });
+mexico.push({ name: "Omarf", salary: 960 });
+mexico.push({ name: "Irving", salary: 500 });
+mexico.push({ name: "Claudia", salary: 873 });
+mexico.push({ name: "Fernando", salary: 1200 });
+mexico.push({ name: "Alberto", salary: 1100 });
+mexico.push({ name: "Tania", salary: 300 });
+mexico.push({ name: "Elvis", salary: 700 });
+mexico.push({ name: "Adán", salary: 1000 });
+mexico.push({ name: "Guadalupe", salary: 780 });
+mexico.push({ name: "Rocío", salary: 500 });
+mexico.push({ name: "Emmanuel", salary: 964 });
+mexico.push({ name: "Vicente", salary: 440 });
+mexico.push({ name: "Joaquín", salary: 573 });
+mexico.push({ name: "Rubén", salary: 900 });
+mexico.push({ name: "Matusalén", salary: 1130 });
+mexico.push({ name: "Raquel", salary: 670 });
+mexico.push({ name: "Elvira", salary: 570 });
+mexico.push({ name: "Alfredo", salary: 400 });
+mexico.push({ name: "Gumercindo", salary: 678 });
+
+const salary = mexico.map(function (persona) {
+  return persona.salary;
+});
+
+const salarySorted = salary.sort(function (salaryA, salaryB) {
+  return salaryA - salaryB;
+});
+
+const medianSalaryGeneral = medianSalary(salarySorted);
+
+const spliceStart = ((salarySorted.length * 90) / 100);
+const spliceCount = salarySorted.length - spliceStart;
+
+const salaryTop10 = salarySorted.splice(
+  spliceStart,
+  spliceCount,
+);
+
+const medianTop10 = medianSalary(salaryTop10);
+
+console.log({
+  medianSalaryGeneral,
+  medianTop10,
+});
+
